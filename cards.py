b179b386acc7b2ec8172655881e14d5fed399132
Nono Zhang <nono.zhang@trantect.com>
2015-12-17 17:51:42 +0800

    first

---
+++ cards.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+color = {'red', 'blue', 'yellow', 'green'}
+
+normal_cards = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+2', 'R', 'ban'
+
+}
+
+no_color = {
+
+}
+
